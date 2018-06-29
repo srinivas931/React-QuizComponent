465529bcb216272124aeb0d824c25513c80e948e
srinivas931 <srinivasreddypakanati93@gmail.com>
2018-06-29 16:11:38 -0400

    Spacing issue

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -9,7 +9,7 @@ class QuizQuestion extends Component {
         }
     }
 
-    handleClick(buttonText){
+    handleClick(buttonText) {
         if(buttonText === this.props.quiz_question.answer){
             this.setState({incorrectAnswer: false})
             this.props.showNextQuestionHandler()

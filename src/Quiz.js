c5d51691a0150d5a706e1399ab10c29072d8ebfc
SP057217 <srinivasreddy.pakanati@cerner.com>
2018-06-29 11:18:12 -0400

    Added click handler logic

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,15 +12,21 @@ class Quiz extends Component {
         }
     }
 
+    showNextQuestion() {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1 }
+        });
+    }
+
     render() {
-        const isQuizEnd = this.state.quiz_position-1 === quizData.quiz_questions.length
+        const isQuizEnd = this.state.quiz_position-1 === quizData.quiz_questions.length;
         return (
         <div>
             {
                 isQuizEnd === true ? <QuizEnd /> :
             <div className="QuizQuestion">
-                <QuizQuestion quiz_question = 
-                {quizData.quiz_questions[this.state.quiz_position - 1]} />
+                <QuizQuestion showNextQuestionHandler = {this.showNextQuestion.bind(this)}
+                            quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]} />
             </div>
             }
         </div>);
